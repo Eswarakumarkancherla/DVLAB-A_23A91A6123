a04e9d64de89631debaa626fb04162023a7bdfdb
Eswarakumarkancherla <keswarkumar2005@gmail.com>
2026-01-22 14:55:04 +0530

    roll no:23A91A6123

---
+++ scripts/WEEK2(2).R
@@ -0,0 +1,23 @@
+# using GGPLOT2 Libraray
+#imporing the library ggplot2
+library(ggplot2)
+
+# A Basic Grid with X and Y axes
+library(ggplot2)
+ggplot(ap_df,
+       aes(x=year,y=passengers))+
+  geom_line(color="darkgreen", linewidth = 0.5)+
+  labs(
+    title = 'Trend Analysis of Air Passengers',
+    subtitle = 'From 1949 - 1960',
+    caption = 'Using Buit-in-Air Passengers dataset',
+    x = 'Months',
+    y = 'no.of Passengers')+
+  geom_point(color = 'red', size = 0.5)+
+  geom_smooth(se=TRUE,color = 'orange')+
+  
+  theme_minimal()+
+  theme(plot.title = element_text(face = "bold", size = 14),
+        plot.subtitle = element_text(size = 10)
+  )
+
